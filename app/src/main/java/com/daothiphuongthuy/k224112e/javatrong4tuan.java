1608549e7a32e648260c224e1e37a0fcf9cfb594
DaoThiPhuongThuy <phuongthuy1622004@gmail.com>
2026-05-02 16:10:25 +0700

    Add full project

---
+++ app/src/main/java/com/daothiphuongthuy/k224112e/javatrong4tuan.java
@@ -0,0 +1,11 @@
+package com.daothiphuongthuy.k224112e;
+
+public class javatrong4tuan {
+    public static void main(String[] args) {
+        System.out.println("Mẹ già chờ con ngồi đếm lá rơi");
+        System.out.println("Đếm bao nhiêu lá\n\tmà con chưa về");
+        System.out.println("Ngày xưa con bé ngây thơ");
+        System.out.println("\tMẹ hay \"ầu yếm\" bên con");
+        System.out.println("\tMẹ bảo mẹ thương con nhiều.");
+    }
+}
